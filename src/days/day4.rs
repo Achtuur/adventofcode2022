3be303474c81599adf5d4ada71988c8dbe7354f4
Achtuur <arthurdegroot88@gmail.com>
2022-12-04 11:24:35 +0100

    updated using flat_map

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 use colored::{ColoredString, Colorize};
+use itertools::Itertools;
 use super::*;
 
 pub struct Day4 {}
@@ -25,58 +26,34 @@ impl AdventDay for Day4 {
             println!("{}", "Input file empty, you probably forgot to copy the input data".bold().red());
         }
 
-        let tot_pairs = input.trim().split('\n').fold(0_u32, |n_pairs, line| {
-            //line = a-b,c-d
-            let elfs = line
-                .trim()
-                .split(',')
-                .map(|elf| { //elf = "x-y"
-                    elf.split('-')
-                        .map(|c| c.parse::<i32>().unwrap())
-                        .collect::<Vec<i32>>()
-                })
-                .collect::<Vec<Vec<i32>>>(); //groups = [[a, b], [c, d]]
+        let pairs = input.trim().split('\n')
+        .flat_map(|line| line.trim().split(',')) //[a-b, c-d]
+        .flat_map(|elf| elf.split('-')) //[a, b, c, d] (as strings)
+        .map(|i| i.parse::<i32>().unwrap())
+        .tuples()
+        .filter(|(a, b, c, d)| a <= c && b >= d || c <= a && d >= b)
+        .count();
 
-            //Check if elf0 is fully overlapped by elf1 or other way around
-            if check_full_overlap(&elfs[0], &elfs[1]) || check_full_overlap(&elfs[1], &elfs[0]) {
-                return n_pairs + 1;
-            }
-            n_pairs
-        });
-        tot_pairs.to_string()
+        pairs.to_string()
     }
 
     fn B(&self, it: &InputType) -> String {
         let input = std::fs::read_to_string(Self::get_path(it))
             .expect("Reading input failed, file doesn't exist most likely");
-        let tot_pairs = input.trim().split('\n').fold(0_u32, |n_pairs, line| {
-            //line = a-b,c-d
-            let elfs = line
-                .trim()
-                .split(',')
-                .map(|elf| { //elf = "x-y"
-                    elf.split('-')
-                        .map(|c| c.parse::<i32>().unwrap())
-                        .collect::<Vec<i32>>()
-                })
-                .collect::<Vec<Vec<i32>>>(); //groups = [[a, b], [c, d]]
 
-            // Check if elf0 partially overlaps into elf1 or other way around
-            if check_partial_overlap(&elfs[0], &elfs[1]) || check_partial_overlap(&elfs[1], &elfs[0])
-            {
-                return n_pairs + 1;
-            }
-            n_pairs
-        });
-        tot_pairs.to_string()
+        let pairs = input.trim().split('\n')
+            .flat_map(|line| line.trim().split(',')) //[a-b, c-d]
+            .flat_map(|elf| elf.split('-')) //[a, b, c, d] (as strings)
+            .map(|i| i.parse::<i32>().unwrap())
+            .tuples()
+            .filter(|(a, b, c, d)| check_partial_overlap(a, b, c, d) || check_partial_overlap(c, d, a, b))
+            .count();
+        pairs.to_string()
     }
 }
 
-///Returns true if elf1 is fully inside elf 2
-fn check_full_overlap(elf1: &[i32], elf2: &[i32]) -> bool {
-    elf1[0] <= elf2[0] && elf1[1] >= elf2[1] // || (elf2[0] <= elf1[0] && elf2[1] >= elf1[1])
-}
-///Returns true if elf1 is partially inside elf 2
-fn check_partial_overlap(elf1: &[i32], elf2: &[i32]) -> bool {
-    elf1[0] >= elf2[0] && elf1[0] <= elf2[1] || elf1[1] >= elf2[0] && elf1[1] <= elf2[1]
+
+///Returns true if elf1 (a,b) is partially inside elf2 (c,d)
+fn check_partial_overlap(a: &i32, b: &i32, c: &i32, d: &i32) -> bool {
+    a >= c && a <= d || b >= c && b <= d
 }
