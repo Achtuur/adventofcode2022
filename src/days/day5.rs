f673ca6d7350f4ab3162eec4056bb74ed03d395f
Achtuur <arthurdegroot88@gmail.com>
2022-12-05 11:02:22 +0100

    6b complete

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -27,20 +27,22 @@ impl AdventDay for Day5 {
             println!("{}", "Input file empty, you probably forgot to copy the input data".bold().red());
         }
 
+        #[cfg(target_os = "windows")]
         let mut input_it = input.split("\r\n\r\n"); //first entry contains initial stack, second entry contains the move commands
+
+        #[cfg(target_os = "linux")]
+        let mut input_it = input.split("\n\n"); //first entry contains initial stack, second entry contains the move commands
         
         let mut queue = read_init_state(input_it.next().unwrap());
         let num_reg = Regex::new(r"\d+").expect("invalid regex");
         input_it.next().unwrap().split('\n').for_each(|op| {
             //args: move args[0] from args[1] to args[2]
             let args = num_reg.find_iter(op).map(|i| i.as_str().parse::<usize>().unwrap()).collect::<Vec<usize>>();
-            println!("stack: {0:?}", queue);
             for _ in 0..args[0] {
                 let a = queue[args[1] - 1].pop_back().unwrap();
                 queue[args[2] - 1].push_back(a);
             }
         });
-        println!("stack: {0:?}", queue);
         queue.iter().fold(String::from(""), |msg, row| format!("{}{}", msg, row[row.len() - 1]))
     }
 
@@ -49,7 +51,26 @@ impl AdventDay for Day5 {
         if input.len() < 3 { //arbitrary small value
             println!("{}", "Input file empty, you probably forgot to copy the input data".bold().red());
         }
-        todo!();
+        #[cfg(target_os = "windows")]
+        let mut input_it = input.split("\r\n\r\n"); //first entry contains initial stack, second entry contains the move commands
+
+        #[cfg(target_os = "linux")]
+        let mut input_it = input.split("\n\n"); //first entry contains initial stack, second entry contains the move commands
+        
+        let mut queue = read_init_state(input_it.next().unwrap());
+        let num_reg = Regex::new(r"\d+").expect("invalid regex");
+        input_it.next().unwrap().split('\n').for_each(|op| {
+            //args: move args[0] from args[1] to args[2]
+            let args = num_reg.find_iter(op).map(|i| i.as_str().parse::<usize>().unwrap()).collect::<Vec<usize>>();
+            let mut a = Vec::<char>::new();
+            for _ in 0..args[0] {
+                a.push(queue[args[1] - 1].pop_back().unwrap());
+            }
+            for i in (0..a.len()).rev() {
+                queue[args[2] - 1].push_back(a[i]);
+            }
+        });
+        queue.iter().fold(String::from(""), |msg, row| format!("{}{}", msg, row[row.len() - 1]))
     }
 }
 
@@ -57,7 +78,8 @@ fn read_init_state(input: &str) -> Vec<VecDeque<char>> {
     let letter_reg = Regex::new(r"[A-Z]").unwrap();
     input.split('\n').fold(Vec::<VecDeque<char>>::new(), |mut queue, line| {
         let mut idx = 0;
-        line.chars().collect::<Vec<char>>().iter().tuples().for_each(|(open, cargo, close, space)| {
+
+        format!("{} ", line).chars().collect::<Vec<char>>().iter().tuples().for_each(|(open, cargo, close, space)| {
             if queue.len() <= idx  {
                 queue.push(VecDeque::<char>::new())
             }
